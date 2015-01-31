ea769154c83cfe72ccf5dc5c8f486d00f583c3b2
Doug Torrance <dtorrance@monmouthcollege.edu>
2015-01-30 20:33:40 -0600

    Imported Upstream version 1.1

--- src/wmgeneral/wmgeneral.c
+++ src/wmgeneral/wmgeneral.c
@@ -98,7 +98,7 @@ int CheckMouseRegion(int, int);
 
 void parse_rcfile(const char *filename, rckeys *keys) {
 
-	char	*p,*q;
+	char	*p,*q, *t;
 	char	temp[128];
 	char	*tokens = " :\t\n";
 	FILE	*fp;
@@ -109,17 +109,19 @@ void parse_rcfile(const char *filename, rckeys *keys) {
 		while (fgets(temp, 128, fp)) {
 			key = 0;
 			q = strdup(temp);
-			q = strtok(q, tokens);
-			while (key >= 0 && keys[key].label) {
-				if ((!strcmp(q, keys[key].label))) {
-					p = strstr(temp, keys[key].label);
-					p += strlen(keys[key].label);
-					p += strspn(p, tokens);
-					if ((i = strcspn(p, "#\n"))) p[i] = 0;
-					free(*keys[key].var);
-					*keys[key].var = strdup(p);
-					key = -1;
-				} else key++;
+			t = strtok(q, tokens);
+			if(t) {
+				while (key >= 0 && keys[key].label) {
+					if ((!strcmp(t, keys[key].label))) {
+						p = strstr(temp, keys[key].label);
+						p += strlen(keys[key].label);
+						p += strspn(p, tokens);
+						if ((i = strcspn(p, "#\n"))) p[i] = 0;
+						free(*keys[key].var);
+						*keys[key].var = strdup(p);
+						key = -1;
+					} else key++;
+				}
 			}
 			free(q);
 		}
